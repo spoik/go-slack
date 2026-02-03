e5d4cabe25702f3b443768a1bcf796b2db6e071f
spike <sebfrye@gmail.com>
2026-02-03 16:42:07 -0600

    Updated the CreateMessage component to show an error message when a message failed to be submitted.

--- frontend/src/components/CreateMessage.vue
+++ frontend/src/components/CreateMessage.vue
@@ -4,9 +4,14 @@ import { createMessage, type Channel } from '@/utils/channel-service'
 
 const props = defineProps<{ channel: Channel }>()
 const message = ref<string>("")
+const errorMessage = ref<string | null>(null)
 
-function createNewMessage() {
-    createMessage(props.channel.id, message.value)
+async function createNewMessage() {
+    try {
+        await createMessage(props.channel.id, message.value)
+    } catch (error: any) {
+        errorMessage.value = "Unable to create message. Please try again."
+    }
 }
 </script>
 
@@ -18,5 +23,7 @@ function createNewMessage() {
 
             <input type="submit" class="bg-indigo-400 text-white px-4 py-2 ml-4 h-auto">
         </form>
+
+        <p v-if="errorMessage != null" class="mt-2 text-red-400" data-error>{{ errorMessage }}</p>
     </div>
 </template>
--- frontend/src/tests/components/CreateMessage.test.ts
+++ frontend/src/tests/components/CreateMessage.test.ts
@@ -3,6 +3,7 @@
 import { mount, type VueWrapper } from '@vue/test-utils'
 import CreateMessage from '@/components/CreateMessage.vue'
 import { type Channel, createMessage } from '@/utils/channel-service'
+import { nextTick } from 'vue'
 
 type CreateMessageWrapper = VueWrapper<InstanceType<typeof CreateMessage>>
 
@@ -21,10 +22,33 @@ describe('CreateMessage component', () => {
 		})
 	})
 
-	it('submits a new message to the provided channel', () => {
-		const message = "New message"
-		wrapper.find('textarea').setValue(message)
-		wrapper.find('form').trigger('submit')
-		expect(createMessage).toBeCalledWith(channel.id, message)
+	describe("submitting the form", () => {
+		async function submit(message: string) {
+			wrapper.find('textarea').setValue(message)
+			wrapper.find('form').trigger('submit')
+			await nextTick()
+		}
+
+		it('submits a new message to the provided channel', async () => {
+			const message = "New message"
+			await submit(message)
+			expect(createMessage).toBeCalledWith(channel.id, message)
+		})
+
+		describe('when the message it successfully created', () => {
+			let errorMessage: string
+
+			beforeEach(() => {
+				errorMessage = "Error message"
+				vi.mocked(createMessage).mockRejectedValue(new Error("Error"))
+			})
+
+			it('shows the error message', async () => {
+				expect(wrapper.find('[data-error]').exists()).toBe(false)
+				await submit("message")
+				const errorElement = wrapper.get('[data-error]')
+				expect(errorElement.text()).toEqual("Unable to create message. Please try again.")
+			})
+		})
 	})
 })
